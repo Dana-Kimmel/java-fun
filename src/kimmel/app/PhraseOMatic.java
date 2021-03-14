72df922bb5368708bd05f428a2d6cf0c4176b55f
Dana Kimmel <dear.prudence34@gmail.com>
2021-03-14 16:58:31 -0400

    init commit

---
+++ src/kimmel/app/PhraseOMatic.java
@@ -0,0 +1,14 @@
+package kimmel.app;
+
+public class PhraseOMatic {
+
+	public static void main(String[] args) {
+		
+		// create three String arrays
+		String[] wordListOne = {"24/7", "multitier", "ginormous", "B-to-B", "win-win", "front-end", "web-based", "pervasive", "smart", "six-sigma",
+				"critical-path", "dynamic"
+		};
+
+	}
+
+}
