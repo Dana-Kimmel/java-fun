d14fcfce5b83f5f09e340197e5795e87e615e96a
Dana Kimmel <dear.prudence34@gmail.com>
2021-03-14 17:14:16 -0400

    added 2nd String array

--- src/kimmel/app/PhraseOMatic.java
+++ src/kimmel/app/PhraseOMatic.java
@@ -3,12 +3,16 @@ package kimmel.app;
 public class PhraseOMatic {
 
 	public static void main(String[] args) {
-		
+
 		// create three String arrays
-		String[] wordListOne = {"24/7", "multitier", "ginormous", "B-to-B", "win-win", "front-end", "web-based", "pervasive", "smart", "six-sigma",
-				"critical-path", "dynamic"
+		String[] wordListOne = { "24/7", "multitier", "ginormous", "B-to-B", "win-win", "front-end", "web-based",
+				"pervasive", "smart", "six-sigma", "critical-path", "dynamic" };
+
+		String[] wordListTwo = { "empowered", "sticky", "value-added", "oriented", "centric", "distributed",
+				"clustered", "branded", "outside-the-box", "positioned", "networked", "focused", "leveraged", "aligned",
+				"targeted", "shared", "cooperative", "accelerated"
+
 		};
 
 	}
-
 }
