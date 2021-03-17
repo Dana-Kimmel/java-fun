d2d88a353fc3f5c41e26da17fff1d6632b068097
Dana Kimmel <dear.prudence34@gmail.com>
2021-03-16 21:14:19 -0400

    started pseudocode

--- stringsplosion/src/kimmel/app/StringSplosionApp.java
+++ stringsplosion/src/kimmel/app/StringSplosionApp.java
@@ -3,8 +3,14 @@ package kimmel.app;
 public class StringSplosionApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
-
+ public String stringSplosion(String str) {
+	
+	 //use the String class's toCharArray method
+	 //use a for loop to go through each character in the array.
+	 
+	 return str;
+ }
 }
