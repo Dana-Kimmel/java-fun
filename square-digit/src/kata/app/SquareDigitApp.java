97df212383016d9094321b300154e83dedd5810d
Dana Kimmel <dear.prudence34@gmail.com>
2021-03-24 22:22:53 -0400

    method

--- square-digit/src/kata/app/SquareDigitApp.java
+++ square-digit/src/kata/app/SquareDigitApp.java
@@ -6,5 +6,15 @@ public class SquareDigitApp {
 		//  square every digit of a number and concatenate them.
 
 	}
-
+	public int squareDigits(int n) {
+		 String result = "";
+		 
+		 while (n != 0) {
+			 int digit = n % 10;
+			 result = digit * digit + result;
+			 n /= 10;
+		 }
+		 return Integer.parseInt(result);
+	}
+       
 }
