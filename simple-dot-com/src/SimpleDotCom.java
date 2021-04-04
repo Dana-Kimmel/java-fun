a8edc68658e83829fec98ea94451e811e92f60fb
Dana Kimmel <dear.prudence34@gmail.com>
2021-04-03 21:38:27 -0400

    checkYourself method added

--- simple-dot-com/src/SimpleDotCom.java
+++ simple-dot-com/src/SimpleDotCom.java
@@ -2,10 +2,29 @@
 public class SimpleDotCom {
 	
 	int[] locationCells;
-	int nomOfHits = 0;
+	int numOfHits = 0;
 	
 	public void setLocationCells(int[] locs) {
 		locationCells = locs;
+	}
+	
+	public String checkYourself(String stringGuess) {
+		int guess = Integer.parseInt(stringGuess);
+		String result = "miss";
+		for (int i =0; i < locationCells.length; i++) {
+			if (guess == locationCells[i]) {
+				result = "hit";
+				numOfHits++;
+				break;
+				
+			}
+		} //out of the loop
+		
+		if (numOfHits == locationCells.length) {
+			result = "kill";
+		}
+		System.out.println(result);
+		return result;
 	}
 	
 
